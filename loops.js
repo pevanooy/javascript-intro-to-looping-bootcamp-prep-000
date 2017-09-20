a0cd6475421c8fe422f191e0f609d713b0f08fb4
Peter van Ooy <pevanooy@gmail.com>
2017-09-20 19:00:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 function forLoop(array){
   for (let i=0; i<25, i++){
-    array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+    if (i==1){
+      array.push(`"I am ${i} strange loop."`);      
+    }
+    else
+      array.push(`"I am ${i} strange loops."`);
+    }
     return array;
   }
 }
